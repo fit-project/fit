4c1c776660244dfb7e86046715621df1be110950
fpollicelli <f.pollicelli@studenti.uniba.it>
2023-01-26 23:46:28 +0100

    first skeleton of mail scraping

--- view/mail.py
+++ view/mail.py
@@ -3,19 +3,19 @@
 ######
 # -----
 # MIT License
-# 
+#
 # Copyright (c) 2022 FIT-Project and others
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
 # the Software without restriction, including without limitation the rights to
 # use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
 # of the Software, and to permit persons to whom the Software is furnished to do
 # so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,40 +24,13 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 # -----
-###### 
+######
 
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
+from controller.mail import Mail as MailController
 
-import sys
-from view.wizard import Wizard as WizardView
-from view.web import Web as WebView
-from view.mail import Mail as MailView
+# definire l'interfaccia
+def Mail():
+    email = input('inserire la mail: ')
+    MailController.get_mails(email)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    wizard = WizardView()
-    wizard.init_wizard()
-    web = WebView()
-    web.hide()
-
-    
-    def start_task(task, case_info):
-        if (task == 'web'):
-            acquisition_window = web
-        elif (task == 'mail'):
-            acquisition_window = MailView()
-        elif (task == 'fb'):
-            pass
-
-        acquisition_window.init(case_info)
-        acquisition_window.show()
-
-    #Wizard sends a signal when finish button is clicked and case is stored on the DB
-    wizard.finished.connect(lambda task, case_info: start_task(task, case_info))
-
-    wizard.show()
-
-    sys.exit(app.exec_())
