ba7d20473053ece90695c0625cdaf2ee8179e54f
fpollicelli <f.pollicelli@studenti.uniba.it>
2023-02-03 16:11:22 +0100

    first implementation for the report generator

---
+++ controller/report.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+######
+# -----
+# MIT License
+#
+# Copyright (c) 2022 FIT-Project and others
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# -----
+######
+from fpdf import FPDF, Template
+class Report:
+    def __init__(self, path, filename):
+        self.path = path
+        self.filename = filename
+
+    def generate_pdf(self, case_info):
+        pdf = FPDF()
+
+        #FIRST PAGE
+        version = "1.0"
+        pdf.add_page()
+        pdf.set_font("Arial", size=40)
+        pdf.cell(200, 100, txt="Freezing Internet Tool",ln=1, align='C')
+        pdf.set_font("Arial", size=30)
+        pdf.cell(200, 10, txt="Report Freezing Internet Tool", ln=2, align='C')
+        pdf.set_font("Arial", size=15)
+        pdf.cell(200, 100, txt="Versione: "+version,ln=2, align='C')
+
+        # SECOND PAGE
+        pdf.cell(200, 100, txt="Fit", ln=1)
+        pdf.set_font("Arial", size=30)
+        pdf.cell(200, 10, txt="IT – Freezing Internet Tool è un’applicazione per l'acquisizione forense di contenuti come pagine web, e-mail e social media direttamente da internet.",
+                 ln=2, align='C')
+
+
+
+
+        # save the pdf with name .pdf
+        pdf.output(self.path+self.filename)
+
+
+    # this will define the ELEMENTS that will compose the template.
+
+#just for testing
+if __name__ == '__main__':
+    path = "C:\\Users\\Routi\\Desktop\\"
+
+    filename = "report.pdf"
+    report = Report(path, filename)
+    report.generate_pdf(None)
