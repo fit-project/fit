8e648debc39fcfc4867392a1f7aef759d3bce63a
Domenico Palmisano <palmisano95lp@gmail.com>
2023-02-08 00:40:14 +0100

    Update scraping Instragram's view

--- view/instagram.py
+++ view/instagram.py
@@ -27,6 +27,8 @@
 ######
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+
 from controller.instagram import Instagram as InstragramController
 
 class Instagram(QtWidgets.QMainWindow):
@@ -38,80 +40,111 @@ class Instagram(QtWidgets.QMainWindow):
 
     def init(self, case_info):
         self.setObjectName("mainWindow")
-        self.resize(580, 311)
+        self.resize(653, 392)
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
         self.input_username = QtWidgets.QLineEdit(self.centralwidget)
-        self.input_username.setGeometry(QtCore.QRect(220, 30, 240, 20))
+        self.input_username.setGeometry(QtCore.QRect(240, 30, 240, 20))
         self.input_username.setObjectName("input_username")
+
         self.input_password = QtWidgets.QLineEdit(self.centralwidget)
-        self.input_password.setGeometry(QtCore.QRect(220, 60, 240, 20))
+        self.input_password.setGeometry(QtCore.QRect(240, 60, 240, 20))
         self.input_password.setObjectName("input_password")
+
         self.label_username = QtWidgets.QLabel(self.centralwidget)
-        self.label_username.setGeometry(QtCore.QRect(60, 30, 100, 20))
+        self.label_username.setGeometry(QtCore.QRect(80, 30, 100, 20))
         self.label_username.setObjectName("label_username")
         self.label_password = QtWidgets.QLabel(self.centralwidget)
-        self.label_password.setGeometry(QtCore.QRect(60, 60, 100, 20))
+        self.label_password.setGeometry(QtCore.QRect(80, 60, 100, 20))
         self.label_password.setObjectName("label_password")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(440, 220, 75, 25))
-        self.pushButton.setObjectName("pushButton")
-        self.input_name = QtWidgets.QLineEdit(self.centralwidget)
-        self.input_name.setGeometry(QtCore.QRect(220, 90, 240, 20))
-        self.input_name.setText("")
-        self.input_name.setObjectName("input_name")
-        self.label_name = QtWidgets.QLabel(self.centralwidget)
-        self.label_name.setGeometry(QtCore.QRect(60, 90, 141, 20))
-        self.label_name.setObjectName("label_name")
-        self.label_username_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_username_2.setGeometry(QtCore.QRect(60, 120, 111, 20))
-        self.label_username_2.setObjectName("label_username_2")
-        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox.setGeometry(QtCore.QRect(240, 150, 70, 17))
-        self.checkBox.setObjectName("checkBox")
-        self.checkBox_2 = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox_2.setGeometry(QtCore.QRect(240, 170, 70, 17))
-        self.checkBox_2.setObjectName("checkBox_2")
-        self.checkBox_3 = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox_3.setGeometry(QtCore.QRect(310, 170, 111, 17))
-        self.checkBox_3.setObjectName("checkBox_3")
-        self.checkBox_4 = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox_4.setGeometry(QtCore.QRect(310, 210, 70, 17))
-        self.checkBox_4.setObjectName("checkBox_4")
-        self.checkBox_5 = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox_5.setGeometry(QtCore.QRect(310, 190, 91, 17))
-        self.checkBox_5.setObjectName("checkBox_5")
-        self.checkBox_6 = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox_6.setGeometry(QtCore.QRect(310, 150, 91, 17))
-        self.checkBox_6.setObjectName("checkBox_6")
-        self.checkBox_7 = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox_7.setGeometry(QtCore.QRect(240, 190, 70, 17))
-        self.checkBox_7.setObjectName("checkBox_7")
-        self.label_username_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_username_3.setGeometry(QtCore.QRect(240, 120, 121, 20))
-        self.label_username_3.setObjectName("label_username_3")
-        self.label_username_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_username_4.setGeometry(QtCore.QRect(60, 150, 111, 20))
-        self.label_username_4.setObjectName("label_username_4")
-        self.label_username_5 = QtWidgets.QLabel(self.centralwidget)
-        self.label_username_5.setGeometry(QtCore.QRect(60, 170, 111, 20))
-        self.label_username_5.setObjectName("label_username_5")
-        self.label_username_6 = QtWidgets.QLabel(self.centralwidget)
-        self.label_username_6.setGeometry(QtCore.QRect(60, 190, 111, 20))
-        self.label_username_6.setObjectName("label_username_6")
-        self.label_username_7 = QtWidgets.QLabel(self.centralwidget)
-        self.label_username_7.setGeometry(QtCore.QRect(60, 210, 111, 20))
-        self.label_username_7.setObjectName("label_username_7")
-        self.label_username_8 = QtWidgets.QLabel(self.centralwidget)
-        self.label_username_8.setGeometry(QtCore.QRect(60, 230, 221, 20))
-        self.label_username_8.setObjectName("label_username_8")
+        self.scrapeButton = QtWidgets.QPushButton(self.centralwidget)
+        self.scrapeButton.setGeometry(QtCore.QRect(520, 270, 75, 25))
+        self.scrapeButton.setObjectName("scrapeButton")
+
+        self.scrapeButton.setEnabled(False)
+        #self.scrapeButton.clicked.connect(self.button_clicked)
+
+        # Verify if username or password are empty
+        self.input_fields = [self.input_username, self.input_password]
+        #for input_field in self.input_fields:
+            #input_field.textChanged.connect(self.onTextChanged)
+
+        self.input_profile = QtWidgets.QLineEdit(self.centralwidget)
+        self.input_profile.setGeometry(QtCore.QRect(240, 90, 240, 20))
+        self.input_profile.setText("")
+        self.input_profile.setObjectName("input_profile")
+        self.label_profile = QtWidgets.QLabel(self.centralwidget)
+        self.label_profile.setGeometry(QtCore.QRect(80, 90, 141, 20))
+        self.label_profile.setObjectName("label_profile")
+        self.label_baseInfo = QtWidgets.QLabel(self.centralwidget)
+        self.label_baseInfo.setGeometry(QtCore.QRect(80, 140, 111, 20))
+        self.label_baseInfo.setObjectName("label_baseInfo")
+
+        self.checkBox_post = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox_post.setGeometry(QtCore.QRect(360, 170, 70, 17))
+        self.checkBox_post.setObjectName("checkBox_post")
+        self.checkBox_post_value = False
+
+        self.checkBox_2_followee = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox_2_followee.setGeometry(QtCore.QRect(360, 190, 70, 17))
+        self.checkBox_2_followee.setObjectName("checkBox_2_followee")
+        self.checkBox_2_followee_value = False
+
+        self.checkBox_3_highlight = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox_3_highlight.setGeometry(QtCore.QRect(430, 190, 111, 17))
+        self.checkBox_3_highlight.setObjectName("checkBox_3_highlight")
+        self.checkBox_3_highlight_value = False
+
+        self.checkBox_4_story = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox_4_story.setGeometry(QtCore.QRect(360, 230, 70, 17))
+        self.checkBox_4_story.setObjectName("checkBox_4_story")
+        self.checkBox_4_story_value = False
+
+        self.checkBox_5_taggedPost = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox_5_taggedPost.setGeometry(QtCore.QRect(430, 210, 91, 17))
+        self.checkBox_5_taggedPost.setObjectName("checkBox_5_taggedPost")
+        self.checkBox_5_taggedPost_value = False
+
+        self.checkBox_6_savedPost = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox_6_savedPost.setGeometry(QtCore.QRect(430, 170, 91, 17))
+        self.checkBox_6_savedPost.setObjectName("checkBox_6_savedPost")
+        self.checkBox_6_savedPost_value = False
+
+        self.checkBox_7_follower = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox_7_follower.setGeometry(QtCore.QRect(360, 210, 70, 17))
+        self.checkBox_7_follower.setObjectName("checkBox_7_follower")
+        self.checkBox_7_follower_value = False
+
+        self.checkBoxes = [self.checkBox_post, self.checkBox_2_followee, self.checkBox_3_highlight, self.checkBox_4_story,
+                           self.checkBox_5_taggedPost, self.checkBox_6_savedPost, self.checkBox_7_follower]
+        #for checkBox in self.checkBoxes:
+            #checkBox.stateChanged.connect(self.checkBox_changed)
+
+        self.label_optionalInfo = QtWidgets.QLabel(self.centralwidget)
+        self.label_optionalInfo.setGeometry(QtCore.QRect(360, 140, 121, 20))
+        self.label_optionalInfo.setObjectName("label_optionalInfo")
+        self.label_completeName = QtWidgets.QLabel(self.centralwidget)
+        self.label_completeName.setGeometry(QtCore.QRect(80, 170, 111, 20))
+        self.label_completeName.setObjectName("label_completeName")
+        self.label_biography = QtWidgets.QLabel(self.centralwidget)
+        self.label_biography.setGeometry(QtCore.QRect(80, 190, 111, 20))
+        self.label_biography.setObjectName("label_biography")
+        self.label_numberOfPost = QtWidgets.QLabel(self.centralwidget)
+        self.label_numberOfPost.setGeometry(QtCore.QRect(80, 210, 111, 20))
+        self.label_numberOfPost.setObjectName("label_numberOfPost")
+        self.label_profileImage = QtWidgets.QLabel(self.centralwidget)
+        self.label_profileImage.setGeometry(QtCore.QRect(80, 230, 111, 20))
+        self.label_profileImage.setObjectName("label_profileImage")
+        self.label_accountType = QtWidgets.QLabel(self.centralwidget)
+        self.label_accountType.setGeometry(QtCore.QRect(80, 250, 221, 20))
+        self.label_accountType.setObjectName("label_accountType")
         self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
-        self.progressBar.setGeometry(QtCore.QRect(450, 260, 118, 23))
+        self.progressBar.setGeometry(QtCore.QRect(517, 340, 131, 23))
         self.progressBar.setProperty("value", 24)
         self.progressBar.setObjectName("progressBar")
         self.setCentralWidget(self.centralwidget)
         self.menuBar = QtWidgets.QMenuBar(self)
-        self.menuBar.setGeometry(QtCore.QRect(0, 0, 580, 21))
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 653, 21))
         self.menuBar.setObjectName("menuBar")
         self.menuConfiguration = QtWidgets.QMenu(self.menuBar)
         self.menuConfiguration.setObjectName("menuConfiguration")
@@ -124,31 +157,45 @@ class Instagram(QtWidgets.QMainWindow):
         self.menuBar.addAction(self.menuCase.menuAction())
         self.menuBar.addAction(self.menuAcquisition.menuAction())
 
-        self.retranslateUi()
+        self.retranslateUi(self)
         QtCore.QMetaObject.connectSlotsByName(self)
 
-    def retranslateUi(self):
+    def retranslateUi(self, mainWindow):
         _translate = QtCore.QCoreApplication.translate
         self.setWindowTitle(_translate("mainWindow", "Freezing Internet Tool"))
         self.label_username.setText(_translate("mainWindow", "Inserisci l\'username"))
         self.label_password.setText(_translate("mainWindow", "Inserisci la password"))
-        self.pushButton.setText(_translate("mainWindow", "Scrape"))
-        self.label_name.setText(_translate("mainWindow", "Inserisci il nome dell\'account"))
-        self.label_username_2.setText(_translate("mainWindow", "Informazioni di base:"))
-        self.checkBox.setText(_translate("mainWindow", "Post"))
-        self.checkBox_2.setText(_translate("mainWindow", "Seguiti"))
-        self.checkBox_3.setText(_translate("mainWindow", "Storie in evidenza"))
-        self.checkBox_4.setText(_translate("mainWindow", "Storie"))
-        self.checkBox_5.setText(_translate("mainWindow", "Post taggati"))
-        self.checkBox_6.setText(_translate("mainWindow", "Post salvati"))
-        self.checkBox_7.setText(_translate("mainWindow", "Seguaci"))
-        self.label_username_3.setText(_translate("mainWindow", "Informazioni aggiuntive:"))
-        self.label_username_4.setText(_translate("mainWindow", "- Nome completo"))
-        self.label_username_5.setText(_translate("mainWindow", "- Biografia"))
-        self.label_username_6.setText(_translate("mainWindow", "- Numero di post"))
-        self.label_username_7.setText(_translate("mainWindow", "- Immagine profilo"))
-        self.label_username_8.setText(_translate("mainWindow", "- Account verificato (si/no) e tipo di account"))
+        self.scrapeButton.setText(_translate("mainWindow", "Scrape"))
+        self.label_profile.setText(_translate("mainWindow", "Inserisci il nome dell\'profilo"))
+        self.label_baseInfo.setText(_translate("mainWindow", "Informazioni di base:"))
+        self.checkBox_post.setText(_translate("mainWindow", "Post"))
+        self.checkBox_2_followee.setText(_translate("mainWindow", "Seguiti"))
+        self.checkBox_3_highlight.setText(_translate("mainWindow", "Storie in evidenza"))
+        self.checkBox_4_story.setText(_translate("mainWindow", "Storie"))
+        self.checkBox_5_taggedPost.setText(_translate("mainWindow", "Post taggati"))
+        self.checkBox_6_savedPost.setText(_translate("mainWindow", "Post salvati"))
+        self.checkBox_7_follower.setText(_translate("mainWindow", "Seguaci"))
+        self.label_optionalInfo.setText(_translate("mainWindow", "Informazioni aggiuntive:"))
+        self.label_completeName.setText(_translate("mainWindow", "- Nome completo"))
+        self.label_biography.setText(_translate("mainWindow", "- Biografia"))
+        self.label_numberOfPost.setText(_translate("mainWindow", "- Numero di post"))
+        self.label_profileImage.setText(_translate("mainWindow", "- Immagine profilo"))
+        self.label_accountType.setText(_translate("mainWindow", "- Account verificato (si/no) e tipo di account"))
         self.menuConfiguration.setTitle(_translate("mainWindow", "Configuration"))
         self.menuCase.setTitle(_translate("mainWindow", "Case"))
         self.menuAcquisition.setTitle(_translate("mainWindow", "Acquisition"))
+
+def button_clicked(self):
+    return
+
+def checkbox_changed(self, state):
+    if state == Qt.Checked:
+        self.checkbox_value = True
+    else:
+        self.checkbox_value = False
+
+def onTextChanged(self):
+    all_field_filled = all(input_field.text() for input_field in self.input_fields)
+    self.scrapeButton.setEnabled(all_field_filled)
+
 
