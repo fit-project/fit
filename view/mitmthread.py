14c6b8569edc4f06a656003ff0cfd49bae98a682
fpollicelli <f.pollicelli@studenti.uniba.it>
2023-02-26 16:14:03 +0100

    adding pem to trusted certs

--- view/mitmthread.py
+++ view/mitmthread.py
@@ -1,14 +1,15 @@
+
+import ssl
 import sys
 import asyncio
-import mitmproxy
-import os
-import tempfile
-from PyQt5.QtCore import QUrl, QObject, pyqtSignal, QThread, QFile, QByteArray
+
+import certifi
+from PyQt5.QtCore import QUrl, QObject, pyqtSignal, QThread
 import logging
 
-from PyQt5.QtNetwork import QNetworkProxy, QSslCertificate, QSslConfiguration, QSsl
-from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile, QWebEnginePage
-from PyQt5.QtWidgets import QApplication, QListView
+from PyQt5.QtNetwork import QNetworkProxy, QSslCertificate, QSslConfiguration
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication
 from mitmproxy.tools import main
 from mitmproxy.tools.dump import DumpMaster
 
@@ -22,7 +23,7 @@ class ProxyServer(QObject):
         self.port = port
 
     async def start(self):
-        # setta le opzioni del proxy
+        # set proxy opts
         options = main.options.Options(listen_host='127.0.0.1', listen_port=self.port,
                                        ssl_insecure=True)
         master = DumpMaster(options=options)
@@ -38,10 +39,10 @@ class MitmThread(QThread):
         self.port = port
 
     def run(self):
-        # nuovo event loop
+        # create new event loop
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        # creazione del mitmproxy
+        # mitmproxy's creation
         proxy_server = ProxyServer(self.port)
         asyncio.get_event_loop().run_until_complete(proxy_server.start())
 
@@ -50,38 +51,49 @@ class MainWindow(QWebEngineView):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        # setta la porta e starta il thread (alla creazione della mainwindow)
+        der_data = set_ssl_config()
+        pem_data = ssl.DER_cert_to_PEM_cert(der_data)
+        self.page().profile().setHttpUserAgent(pem_data)
+
+        # set port and start the thread (while creating the mainwindow)
         port = 8081
         self.mitm_thread = MitmThread(port)
         self.mitm_thread.start()
 
-        super().load(QUrl("https://www.google.com/"))
+        url = QUrl("https://www.google.com/")
+        super().load(url)
+
+
+def set_ssl_config():
+    # get the pem with openssl from the mitmproxy .p12
+    # openssl pkcs12 -in {mitmproxy-ca-cert.p12} -out {cert_pem} -nodes
+
+    # add pem to the trusted root certificates (won't solve the problem)
+    cert_file_path = certifi.where()
+    pem_path= 'C:\\Users\\Routi\\Downloads\\mitmproxy-ca-cert.pem'
+    with open(pem_path, 'rb') as pem_file:
+        pem_data = pem_file.read()
+    with open(cert_file_path, 'ab') as cert_file:
+        cert_file.write(pem_data)
+
+    # create a QSslConfiguration object with the certificate as the CA certificate (won't solve the problem)
+    cert = QSslCertificate(pem_data)
+    config = QSslConfiguration.defaultConfiguration()
+    config.setCaCertificates([cert])
+
+    # get the DER-encoded certificate data
+    der_data = cert.toDer()
+    return der_data
 
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    # directly getting the the pem with openssl solved the problem
-    # openssl pkcs12 -in {mitmproxy-ca-cert.p12} -out {cert_pem} -nodes
-
-    with open('C:\\Users\\Routi\\Downloads\\mitmproxy-ca-cert.pem', 'rb') as f:
-        pem_data = f.read()
-
-    cert = QSslCertificate.fromData(pem_data)
-    print(cert[0])
-
-    # Create a custom SSL configuration that includes the mitmproxy certificate
-    config = QSslConfiguration.defaultConfiguration()
-    config.setCaCertificates(cert)
-    config.setProtocol(QSsl.TlsV1_2)  # Set the SSL protocol to use (optional)
-
-
-    view = MainWindow()
-    view.show()
-    # crea un QNetworkProxy e lo setta come proxy
+    webview = MainWindow()
+    # create the proxy
     proxy = QNetworkProxy(QNetworkProxy.HttpProxy, '127.0.0.1', 8081)
     QNetworkProxy.setApplicationProxy(proxy)
 
-    view.show()
+    webview.show()
     sys.exit(app.exec_())
