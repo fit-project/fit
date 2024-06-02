5d799d388e9af31d1f01d6b8022705cb07bbebaf
Fabio Zito <zitelog@gmail.com>
2024-06-02 13:50:00 +0200

    Refactoring Case Dialog UI

--- view/case_form_dialog.py
+++ view/case_form_dialog.py
@@ -10,9 +10,7 @@
 from PyQt6 import QtCore, QtWidgets, uic
 
 from view.case_form_manager import CaseFormManager
-
-
-from common.utility import resolve_path
+from common.utility import resolve_path, get_version
 
 from controller.case import Case as CaseController
 from common.constants.view.case import *
@@ -22,11 +20,31 @@ class CaseFormDialog(QtWidgets.QDialog):
     def __init__(self, case_info=None, parent=None):
         super(CaseFormDialog, self).__init__(parent)
 
-        uic.loadUi(resolve_path("ui/case/dialog.ui"), self)
+        self.__case_info = case_info
 
-        self.case_info = case_info
+        self.__init_ui()
 
-        case_name = self.case_info.get("name")
+    def __init_ui(self):
+        uic.loadUi(resolve_path("ui/case/case.ui"), self)
+
+        # HIDE STANDARD TITLE BAR
+        self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
+
+        # SET TOP BAR
+        self.topBarLeftBox.mouseMoveEvent = self.move_window
+        self.minimizeButton.clicked.connect(self.showMinimized)
+        self.closeButton.clicked.connect(self.close)
+
+        # SET VERSION
+        self.version.setText("v" + get_version())
+
+        # CANCEL BUTTON
+        self.cancel_button.clicked.connect(self.reject)
+        # SAVE BUTTON
+        self.save_button.clicked.connect(self.accept)
+
+        case_name = self.__case_info.get("name")
         self.form_manager = CaseFormManager(self.findChild(QtWidgets.QFrame, "form"))
 
         self.name = self.findChild(QtWidgets.QComboBox, "name")
@@ -34,23 +52,29 @@ class CaseFormDialog(QtWidgets.QDialog):
         self.name.lineEdit().setReadOnly(True)
 
         if case_name not in self.form_manager.controller.names:
-            self.form_manager.cases.append(self.case_info)
+            self.form_manager.cases.append(self.__case_info)
 
         id = -1
-        if "id" in self.case_info:
-            id = self.case_info.get("id")
+        if "id" in self.__case_info:
+            id = self.__case_info.get("id")
 
-        self.setWindowTitle(DIALOG_TITLE.format(case_name, str(id)))
-        self.setWindowFlags(
-            self.windowFlags() & ~QtCore.Qt.WindowType.WindowContextHelpButtonHint
-        )
+        self.titleRightInfo.setText(DIALOG_TITLE.format(case_name, str(id)))
 
         self.form_manager.set_case_information()
 
-    def accept(self) -> None:
-        self.case_info = self.form_manager.get_current_case_info()
-        CaseController().cases = self.case_info
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPosition().toPoint()
+
+    def move_window(self, event):
+        if event.buttons() == QtCore.Qt.MouseButton.LeftButton:
+            self.move(self.pos() + event.globalPosition().toPoint() - self.dragPos)
+            self.dragPos = event.globalPosition().toPoint()
+            event.accept()
+
+    def accept(self):
+        self.__case_info = self.form_manager.get_current_case_info()
+        CaseController().cases = self.__case_info
         return super().accept()
 
-    def reject(self) -> None:
+    def reject(self):
         return super().reject()
