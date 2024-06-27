6befe0ae09df97ae20d635d96ab420bcdf71079d
Fabio Zito <zitelog@gmail.com>
2024-06-27 11:30:20 +0200

    Refactoring Done

--- view/wizard.py
+++ view/wizard.py
@@ -10,7 +10,7 @@
 
 from configparser import ConfigParser
 
-from PyQt6 import QtCore, QtWidgets, QtGui, uic
+from PyQt6 import QtCore, QtWidgets, uic
 
 from ui.wizard import resources
 
@@ -24,9 +24,7 @@ from common.utility import resolve_path
 
 from common.constants.view.wizard import *
 from common.constants import error
-from common.constants.view import wizard, general
-
-MAX_WIDTH_QCHECKBOX_TEXT = 120
+from common.constants.view import wizard
 
 
 class Wizard(QtWidgets.QMainWindow):
@@ -43,38 +41,26 @@ class Wizard(QtWidgets.QMainWindow):
         self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
 
-        # GET CUSTOM BAR
-        self.custom_bar = self.findChild(QtWidgets.QFrame, "leftBox")
-        self.custom_bar.mouseMoveEvent = self.move_window
+        # CUSTOM TOP BAR
+        self.left_box.mouseMoveEvent = self.move_window
 
-        # SETTING BUTTON
-        self.configuration_button = self.findChild(
-            QtWidgets.QPushButton, "settingsTopButton"
-        )
+        # CONFIGURATION BUTTON
         self.configuration_button.clicked.connect(self.__configuration)
 
         # MINIMIZE BUTTON
-        self.minimize_app_button = self.findChild(
-            QtWidgets.QPushButton, "minimizeButton"
-        )
-        self.minimize_app_button.clicked.connect(lambda: self.showMinimized())
+        self.minimize_button.clicked.connect(lambda: self.showMinimized())
 
         # CLOSE BUTTON
-        self.close_app_button = self.findChild(QtWidgets.QPushButton, "closeButton")
-        self.close_app_button.clicked.connect(lambda: self.close())
+        self.close_button.clicked.connect(lambda: self.close())
 
         # PAGES
-        self.pages = self.findChild(QtWidgets.QStackedWidget, "pages")
         self.pages.setCurrentIndex(0)
-        self.navigation_buttons = self.findChild(QtWidgets.QFrame, "navigationButtons")
 
         # NEXT BUTTON
-        self.next_button = self.findChild(QtWidgets.QPushButton, "nextButton")
         self.next_button.setEnabled(False)
         self.next_button.clicked.connect(lambda: self.__next_page())
 
         # BACK BUTTON
-        self.back_button = self.findChild(QtWidgets.QPushButton, "backButton")
         self.back_button.clicked.connect(lambda: self.__back_page())
         self.back_button.hide()
 
@@ -83,46 +69,17 @@ class Wizard(QtWidgets.QMainWindow):
         self.version.setText("v" + self.__get_version())
 
         # PAGE1 CASE INFO FORM
-        self.form_manager = CaseFormManager(self.findChild(QtWidgets.QFrame, "form"))
+        self.form_manager = CaseFormManager(self.form)
         self.form_manager.name.currentTextChanged.connect(self.__enable_next_button)
+
         # PAGE2 SELECT TASK
         self.tasks = self.findChildren(QtWidgets.QCheckBox)
-
-        current_max_width_qcheckbox_text = 0
-        current_max_qcheckbox_text = None
-        current_max_qcheckbox_font = None
 
         for task in self.tasks:
             task.setAttribute(QtCore.Qt.WidgetAttribute.WA_MacShowFocusRect, 0)
             task.clicked.connect(self.__task_clicked)
-            current_width_text = self.__calculate_width_from_text(
-                task.font(), task.text()
-            )
-            if current_width_text > MAX_WIDTH_QCHECKBOX_TEXT:
-                if current_width_text > current_max_width_qcheckbox_text:
-                    current_max_width_qcheckbox_text = current_width_text
-                    current_max_qcheckbox_text = task.text()
-                    current_max_qcheckbox_font = task.font()
-
-        if current_max_width_qcheckbox_text > MAX_WIDTH_QCHECKBOX_TEXT:
-            while current_max_width_qcheckbox_text > MAX_WIDTH_QCHECKBOX_TEXT:
-                current_font_size = current_max_qcheckbox_font.pointSize()
-                current_max_qcheckbox_font.setPointSize(current_font_size - 1)
-                current_max_width_qcheckbox_text = self.__calculate_width_from_text(
-                    current_max_qcheckbox_font, current_max_qcheckbox_text
-                )
-
-            for task in self.tasks:
-                task.setStyleSheet(
-                    "QCheckBox {font-size:"
-                    + str(current_max_qcheckbox_font.pointSize())
-                    + "px; }"
-                )
 
         # SUMMARY BUTTON
-        self.case_summary_button = self.findChild(
-            QtWidgets.QPushButton, "caseSummaryButton"
-        )
         self.case_summary_button.clicked.connect(self.__case_summary)
         self.case_summary_button.hide()
 
@@ -140,11 +97,6 @@ class Wizard(QtWidgets.QMainWindow):
             self.move(self.pos() + event.globalPosition().toPoint() - self.dragPos)
             self.dragPos = event.globalPosition().toPoint()
             event.accept()
-
-    def __calculate_width_from_text(self, font, text):
-        fm = QtGui.QFontMetrics(font)
-
-        return fm.horizontalAdvance(text)
 
     def __next_page(self):
         if self.pages.currentIndex() == 0:
@@ -225,13 +177,3 @@ class Wizard(QtWidgets.QMainWindow):
         version = parser.get("fit_properties", "tag_name")
 
         return version
-
-
-if __name__ == "__main__":
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    app.setWindowIcon(QtGui.QIcon(resolve_path("icon.ico")))
-    MainWindow = Wizard()
-    MainWindow.show()
-    sys.exit(app.exec())
