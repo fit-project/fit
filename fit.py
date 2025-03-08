6841c4b4cc478d4a90184e992fcba56fc79042d9
Fabio Zito <zitelog@gmail.com>
2025-03-08 10:01:13 +0100

    refactoring

--- fit.py
+++ fit.py
@@ -6,10 +6,13 @@
 # SPDX-License-Identifier: GPL-3.0-only
 # -----
 ######
+
 import sys
 import ctypes
 import os
-import signal
+import stat
+import sys
+import subprocess
 
 from PyQt6 import QtWidgets, QtGui
 from common.utility import resolve_path, get_platform
@@ -22,6 +25,40 @@ from view.scrapers.video.video import Video as VideoView
 from view.scrapers.entire_website.entire_website import (
     EntireWebsite as EntireWebsiteView,
 )
+
+
+def get_shared_temp_dir():
+    """Restituisce una cartella temporanea accessibile da tutti gli utenti."""
+    if get_platform() == "win":
+        return "C:\\ProgramData\\fit-bootstrap"
+    else:
+        return "/var/tmp/fit-bootstrap"
+
+
+def notify_fit_bootstrap(fit_bootstrap_pid):
+    """Crea il file fit_ready_<pid> nella cartella condivisa e imposta i permessi per renderlo cancellabile da tutti."""
+    temp_dir = get_shared_temp_dir()
+    os.makedirs(temp_dir, exist_ok=True)  # Crea la cartella se non esiste
+    ready_file = os.path.join(temp_dir, f"fit_ready_{fit_bootstrap_pid}")
+
+    try:
+        with open(ready_file, "w") as f:
+            f.write("ready")
+        print(f"File {ready_file} creato con successo.")
+
+        if get_platform() == "win":
+            # Modifica i permessi su Windows per rendere il file cancellabile da tutti
+            subprocess.run(["icacls", ready_file, "/grant", "Everyone:F"], check=True)
+            print(f"Permessi aggiornati per {ready_file} su Windows.")
+        else:
+            # Modifica i permessi su macOS/Linux per rendere il file cancellabile da tutti
+            os.chmod(
+                ready_file, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO
+            )  # chmod 777
+            print(f"Permessi aggiornati per {ready_file} su macOS/Linux.")
+
+    except Exception as e:
+        print(f"Errore nella creazione del file {ready_file}: {e}")
 
 
 class FitApplication(QtWidgets.QApplication):
@@ -39,7 +76,7 @@ class FitWizard(WizardView):
         super().showEvent(event)
         if self.fit_bootstrap_pid:
             print("Finestra di Fit visibile! Notifico fit-bootstrap...")
-            os.kill(self.fit_bootstrap_pid, signal.SIGTERM)
+            notify_fit_bootstrap(self.fit_bootstrap_pid)
 
 
 if __name__ == "__main__":
