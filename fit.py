913d3d4a4720da00e770f581def98ca5c3e9074d
Fabio Zito <zitelog@gmail.com>
2025-10-13 13:53:15 +0200

    feat: add wizard and web scraper modules, update poetry, and add update-version workflow
    - Implement wizard and web scraper modules
    - Update Poetry dependencies
    - Add update-version GitHub Actions workflow

--- fit.py
+++ fit.py
@@ -7,24 +7,75 @@
 # -----
 ######
 
+import argparse
 import sys
 
-from fit_common.core import resolve_path
+from fit_common.core import (
+    DebugLevel,
+    debug,
+    resolve_path,
+    set_debug_level,
+    set_gui_crash_handler,
+)
+from fit_web.web import Web
 from fit_wizard.view.wizard import Wizard
 from PySide6 import QtGui, QtWidgets
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="FIT")
+    parser.add_argument(
+        "--debug",
+        choices=["none", "log", "verbose"],
+        default="none",
+        help="Set the debug level (default: none)",
+    )
+    return parser.parse_args()
+
+
+def show_crash_dialog(error_message: str):
+    msg_box = QtWidgets.QMessageBox()
+    msg_box.setIcon(QtWidgets.QMessageBox.Critical)
+    msg_box.setWindowTitle("Application Error")
+    msg_box.setText("A fatal error occurred:")
+    msg_box.setDetailedText(error_message)
+    msg_box.setStandardButtons(QtWidgets.QMessageBox.Ok)
+    msg_box.exec()
+
+
 def main():
-    def start_task(task, case_info):
-        print(f"task: {task}")
-        print(f"case_info: {case_info}")
-        window.close()
+    def start_task():
+        task = None
+        if wizard.selcted_task == "web":
+            task = Web(wizard)
+
+        if task is not None and task.has_valid_case:
+            wizard.hide()
+            task.show()
+        else:
+            debug(
+                "User cancelled the case form. Nothing to display.",
+                context="Main.fit_web",
+            )
+            sys.exit(0)
+
+    args = parse_args()
+    set_debug_level(
+        {
+            "none": DebugLevel.NONE,
+            "log": DebugLevel.LOG,
+            "verbose": DebugLevel.VERBOSE,
+        }[args.debug]
+    )
 
     app = QtWidgets.QApplication(sys.argv)
     app.setWindowIcon(QtGui.QIcon(resolve_path("icon.ico")))
-    window = Wizard()
-    window.finished.connect(lambda task, case_info: start_task(task, case_info))
-    window.show()
+
+    set_gui_crash_handler(show_crash_dialog)
+
+    wizard = Wizard()
+    wizard.finished.connect(start_task)
+    wizard.show()
     sys.exit(app.exec())
 
 
